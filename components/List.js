5824a7b76fa234d21064ab03d8979bf5f3f31c3b
bhartibharti <bhar0041@algonquinlive.com>
2020-12-18 21:13:38 +0530

    Removed Proxy and Platform

--- components/List.js
+++ components/List.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react'
 import {
   ActivityIndicator,
   Image,
@@ -7,82 +7,78 @@ import {
   Text,
   TouchableOpacity,
   Platform
-} from "react-native";
-import { FlatList } from "react-native-gesture-handler";
+} from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
 
-export default function List({ navigation, route }) {
+export default function List ({ navigation, route }) {
   const Bearer =
-    "45mEth91SGrrTZghjhUK6s--hPksQPzZUpo4tgMwC3IXTY2C3ReP7BWBLupqNBBZxR-qt593a0-fSQ0sMXthd5C5uJ835k6GKZL_GTK4D6eK86oCHx86dNlA8fbOX3Yx";
-  const [list, setList] = useState(null);
+    '45mEth91SGrrTZghjhUK6s--hPksQPzZUpo4tgMwC3IXTY2C3ReP7BWBLupqNBBZxR-qt593a0-fSQ0sMXthd5C5uJ835k6GKZL_GTK4D6eK86oCHx86dNlA8fbOX3Yx'
+  const [list, setList] = useState(null)
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      geoSuccess,
-      geoFailure,
-      geoOptions
-    );
-  }, []);
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoFailure, geoOptions)
+  }, [])
 
   let geoOptions = {
     enableHighAccuracy: true,
     timeOut: 20000,
-    maximumAge: 60 * 60,
-  };
-
-  function geoSuccess(pos) {
-    var loc = pos.coords;
-    // console.log(loc.latitude, loc.longitude)
-    fetchList(loc.latitude, loc.longitude).then((data) =>
-      setList(data.businesses)
-      // console.log(`data is: ${data}`)
-    );
+    maximumAge: 60 * 60
   }
 
-  async function fetchList(lat, lng) {
-    const proxy = "https://cors-anywhere.herokuapp.com/";
-    const url = `https://api.yelp.com/v3/businesses/search?latitude=${lat}&longitude=${lng}`;
-    const final = Platform.OS === 'android' ? url : proxy+url
+  function geoSuccess (pos) {
+    var loc = pos.coords
+    // console.log(loc.latitude, loc.longitude)
+    fetchList(loc.latitude, loc.longitude).then(
+      data => setList(data.businesses)
+      // console.log(`data is: ${data}`)
+    )
+  }
+
+  async function fetchList (lat, lng) {
+    // const proxy = "https://cors-anywhere.herokuapp.com/";
+    const url = `https://api.yelp.com/v3/businesses/search?latitude=${lat}&longitude=${lng}`
+    // const final = Platform.OS === 'android' ? url : proxy+url
     // const url = `https://api.coinlore.net/api/tickers/`;
     try {
-      const res = await fetch(final, {
-        method: "GET",
+      const res = await fetch(url, {
+        method: 'GET',
         headers: new Headers({
-          Authorization: `Bearer ${Bearer}`,
-        }),
-      });
-      if (!res.ok) throw new Error(res.statusText);
-      return res.json();
+          Authorization: `Bearer ${Bearer}`
+        })
+      })
+      if (!res.ok) throw new Error(res.statusText)
+      return res.json()
     } catch (err) {
-      console.error(err);
+      console.error(err)
     }
   }
-  function geoFailure(pos) {
-    console.warn(`ERROR: ${pos}`);
+  function geoFailure (pos) {
+    console.warn(`ERROR: ${pos}`)
   }
 
   if (!list)
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <ActivityIndicator size="large" color="#B40000" />
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <ActivityIndicator size='large' color='#B40000' />
       </View>
-    );
+    )
 
-  console.log(list);
+  console.log(list)
 
-  let orderedList = list.sort((a, b) => a.distance - b.distance);
+  let orderedList = list.sort((a, b) => a.distance - b.distance)
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <FlatList
         data={orderedList}
         renderItem={({ item }) => (
           <TouchableOpacity
             // style={styles.button}
             onPress={() => {
-              navigation.navigate("Details", {
+              navigation.navigate('Details', {
                 alias: item.alias,
-                distance: (item.distance / 1000).toFixed(2),
-              });
+                distance: (item.distance / 1000).toFixed(2)
+              })
             }}
           >
             <Image
@@ -110,22 +106,22 @@ export default function List({ navigation, route }) {
         );
       })} */}
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   button: {
-    alignItems: "center",
-    backgroundColor: "#B40000",
+    alignItems: 'center',
+    backgroundColor: '#B40000',
     padding: 20,
-    borderRadius: 12,
+    borderRadius: 12
   },
   text: {
-    color: "white",
-    fontSize: 30,
+    color: 'white',
+    fontSize: 30
   },
   textOut: {
     paddingTop: 10,
-    fontSize: 16,
-  },
-});
+    fontSize: 16
+  }
+})
