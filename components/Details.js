a51ee19f2d1078cbb589e416c8f8bc92bfce9fbc
Vuong Tuan Kiet <55266028+vuon0029@users.noreply.github.com>
2020-12-18 14:03:14 -0500

    fea: MapView & style: Styled Details screen

--- components/Details.js
+++ components/Details.js
@@ -1,18 +1,31 @@
 import React, { useState, useEffect } from "react";
-import { Platform, ActivityIndicator, Image, View, Text } from "react-native";
+import {
+  StyleSheet,
+  Platform,
+  ActivityIndicator,
+  Image,
+  View,
+  Text,
+} from "react-native";
+import RatingImage from "./RatingImage";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Icon } from "react-native-elements";
 
 export default function Details({ route }) {
   const Bearer =
     "45mEth91SGrrTZghjhUK6s--hPksQPzZUpo4tgMwC3IXTY2C3ReP7BWBLupqNBBZxR-qt593a0-fSQ0sMXthd5C5uJ835k6GKZL_GTK4D6eK86oCHx86dNlA8fbOX3Yx";
   const [details, setDetails] = useState(null);
+  const [reviews, setReviews] = useState(null)
 
   useEffect(() => {
-    fetchDetails().then((data) => setDetails(data));
+    fetchDetails(`https://api.yelp.com/v3/businesses/${route.params.alias}`).then((data) => setDetails(data));
+    fetchDetails(`https://api.yelp.com/v3/businesses/${route.params.alias}/reviews`).then((data) => setReviews(data.reviews));
+
     console.log(route.params.alias);
-    async function fetchDetails() {
+    async function fetchDetails(url) {
       const proxy = "https://cors-anywhere.herokuapp.com/";
-      const url = `https://api.yelp.com/v3/businesses/${route.params.alias}`
-      const final = Platform.OS === 'android' ? url : proxy+url
+      // const url = `https://api.yelp.com/v3/businesses/${route.params.alias}`;
+      const final = Platform.OS === "android" ? url : proxy + url;
       try {
         const res = await fetch(final, {
           method: "GET",
@@ -28,7 +41,7 @@ export default function Details({ route }) {
     }
   }, [route.params.alias]);
 
-  if (!details)
+  if (!details || !reviews)
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <ActivityIndicator size="large" color="#B40000" />
@@ -36,18 +49,184 @@ export default function Details({ route }) {
     );
 
   console.log(details);
+  console.log(reviews)
+  let priceRange = ["Cheap", "Fair", "Expensive", "Very Expensive"]
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Image
-        source={{ uri: details.image_url }}
-        style={{ width: 200, height: 200, backgroundColor: details.image_url==""? "#B3B3B3" : "transparent"  }}
-      />
-      <Text>Name: {details.name}</Text>
-      <Text>Phone: {details.phone}</Text>
-      <Text>Distance: {route.params.distance}km</Text>
-      <Text>Price: {details.price}</Text>
-      <Text>Rating: {details.rating}</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }} edges={["right", "bottom", "left"]}>
+      <View
+        style={{ flex: 1, alignItems: "center", justifyContent: "flex-start" }}
+      >
+        <View>
+          <Image
+            source={{ uri: details.photos[1] }}
+            style={{
+              width: 400,
+              height: 150,
+            }}
+          />
+          <View style={styles.overlay} />
+        </View>
+        <View style={styles.avt}>
+          <Image
+            source={{ uri: details.image_url }}
+            style={{
+              width: 120,
+              height: 120,
+              backgroundColor: details.image_url == "" ? "#B3B3B3" : "white",
+              borderRadius: 60,
+            }}
+          />
+        </View>
+        <Text style={styles.name}>{details.name}</Text>
+        <RatingImage item={details.rating} />
+        <View style={styles.info}>
+          <Text style={styles.head}>Information</Text>
+          <View style={styles.details}>
+            <Text style={styles.text}>
+              <Icon style={styles.icon} name="phone" color="#B40000" />
+
+              {details.phone}
+            </Text>
+            <Text style={styles.text}>
+              <Icon
+                style={styles.icon}
+                name="truck"
+                color="#B40000"
+                type="font-awesome"
+              />
+              {route.params.distance}km away
+            </Text>
+            <Text style={styles.text}>
+              <Icon
+                style={styles.icon}
+                name="cash-outline"
+                color="#B40000"
+                type="ionicon"
+              />
+              {details.price? priceRange[details.price.length] : null}
+            </Text>
+
+            <View style={{alignItems: "flex-start"}}>
+              <Icon
+                style={styles.icon}
+                name="chatbox-ellipses-outline"
+                color="#B40000"
+                type="ionicon"
+              />
+              <View>
+              {reviews.map((item)=>{
+                return (
+                  <View style={styles.review}>
+                    <Text style={styles.username}>{item.user.name}</Text>
+                    <Text style={styles.desc}>{item.text}</Text>
+                  </View>
+                )
+              })}
+              </View>
+            </View>
+
+            {/* <Text style={styles.text}>
+              <Icon
+                style={styles.icon}
+                name="chatbox-ellipses-outline"
+                color="#B40000"
+                type="ionicon"
+              />
+            </Text> */}
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  avt: {
+    width: 130,
+    height: 130,
+    backgroundColor: "#CE1E1E",
+    padding: 5,
+    margin: 20,
+    borderRadius: 70,
+    marginTop: -90,
+  },
+  review: {
+    // flex: 1,
+    // flexDirection: "row",
+    // margin: 35,
+    // borderColor: "#B40000",
+    // borderStyle: "solid",
+    // backgroundColor: "#FFFFFF",
+    // borderBottomColor: "#B40000",
+    // borderWidth: 1,
+    margin: 2
+    // shadowColor: "#000",
+    // shadowOffset: {
+    //   width: 0,
+    //   height: 5,
+    // },
+    // shadowOpacity: 0.36,
+    // shadowRadius: 6.68,
+
+    // elevation: 11,
+  },
+  username: {
+    fontSize: 16,
+    fontWeight: "bold",
+    padding: 2
+  },
+  desc: {
+    fontSize: 14,
+    padding: 2
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  name: {
+    fontSize: 24,
+    fontWeight: "300",
+    marginTop: -25,
+  },
+  info: {
+    flex: 1,
+    width: 320,
+    margin: 35,
+    marginBottom: 0,
+    borderRadius: 12,
+    borderColor: "#B40000",
+    borderStyle: "solid",
+    backgroundColor: "#FFFFFF",
+    borderWidth: 3,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
+
+    elevation: 11,
+  },
+  details: {
+    padding: 10,
+  },
+  text: {
+    fontSize: 18,
+    paddingLeft: 12,
+  },
+  price: {
+    color: "#B40000",
+    fontSize: 26,
+  },
+  head: {
+    fontSize: 26,
+    margin: 10,
+    fontWeight: "200",
+    // backgroundColor: "#FFFFFF"
+  },
+  icon: {
+    marginRight: 12
+  }
+});
--- components/Home.js
+++ components/Home.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import { StyleSheet, View, Text, Button, TouchableOpacity } from 'react-native'
+import React from "react";
+import { StyleSheet, View, Text, Button, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function Home({navigation, route}) {
+export default function Home({ navigation, route }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <TouchableOpacity 
-        style={styles.button}
-        onPress={()=> {navigation.navigate("List")}}>
-        <Text
-          style={styles.text}>Restaurants nearby</Text>
-      </TouchableOpacity>
-      <Text style={styles.textOut}>Get a list of all the restaurants near you.</Text>
-    </View>
-  )
+    <SafeAreaView style={{ flex: 1 }} edges={["right", "bottom", "left"]}>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            navigation.navigate("List");
+          }}
+        >
+          <Text style={styles.text}>Restaurants nearby</Text>
+        </TouchableOpacity>
+        <Text style={styles.textOut}>
+          Get a list of all the restaurants near you.
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -20,15 +27,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#B40000",
     padding: 20,
-    borderRadius: 12
+    borderRadius: 12,
   },
   text: {
     color: "white",
     fontSize: 26,
-    fontWeight: "300"
+    fontWeight: "300",
   },
   textOut: {
     paddingTop: 10,
     fontSize: 16,
-  }
-})
+  },
+});
